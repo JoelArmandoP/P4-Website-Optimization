e02e11e49f18b92a6bc28b1509b4d1b9a16bac15
Joel Armando <joelarma@gmail.com>
2015-10-18 13:56:06 +0100

    Implement inline, uglify and imagemin in pizza.html, project-mobile, project-webperf.html.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,77 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-/*    responsive_images: {
-      dev: {
-        options: { engine: "im"},
-        sizes: [{
-          name: 'small',
-          width: '30%',
-          suffix: '_small',
-          quality: 20
-        },{
-          name: 'large',
-          width: '50%',
-          suffix: '_large',
-          quality: 40
-        }],
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images-src/',
-          dest: 'images/'
-        }]
-      }
-    }, */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'img/',
-          dest: 'dist/img/'
-        },
-        {
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'views/images/',
-          dest: 'dist/views/images/'
-        },
-        {
-          expand: true,
-          src: ['*'],
-          cwd: 'css/',
-          dest: 'dist/css/'
-        },
-        {
-          expand: true,
-          src: ['*'],
-          cwd: 'js/',
-          dest: 'dist/js/'
-        }]
-      }
-    },
-    inline: {
-      dist: {
-        options:{
-          cssmin: true
-        },
-        expand: true,
-        src: ['*.html'],
-        cwd: './',
-        dest: 'dist/'
-      }
-    }
-  });
+    grunt.initConfig({
+	inline: {
+	    dist: {
+		options:{
+		    cssmin: true
+		},
+		files: [
+		    {
+			expand: true,
+			src: ['**/*.html'],
+			cwd: './',
+			dest: 'dist/'
+		    }]
+	    }
+	},
+	imagemin: {
+            dist: {
+		options: {
+		    optimizationLevel: 7
+		},
+		files: [
+		    {
+			expand: true,
+			src: ['**/*.{png,jpg,gif}'],
+			cwd: 'img/',
+			dest: 'dist/img/'
+		    }, {
+			expand: true,
+			src: ['**/*.{png,jpg,gif}'],
+			cwd: 'views/images/',
+			dest: 'dist/views/images/'
+		    }]
+            }
+	},
+	uglify: {
+            dist: {
+		files: [
+		    {
+			expand: true,
+			src: ['*.js'],
+			cwd: 'js/',
+			dest: 'dist/js/'
+		    }, {
+			expand: true,
+			src: ['*.js'],
+			cwd: 'views/js/',
+			dest: 'dist/views/js/'
+		    }]
+            }
+	},
+	cssmin: {
+            dist: {
+		files: [
+		    {
+			expand: true,
+			src: ['*.css'],
+			cwd: 'css/',
+			dest: 'dist/css/'
+		    }, {
+			expand: true,
+			src: ['*.css'],
+			cwd: 'views/css/',
+			dest: 'dist/views/css/'
+		    }]
+            }
+	}
+    });
 
-  //grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-inline');
-  grunt.registerTask('default', ['responsive_images', 'copy']);
 
-};
+    grunt.loadNpmTasks('grunt-inline');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('default', ['inline', 'uglify', 'cssmin', 'imagemin']);
+}
