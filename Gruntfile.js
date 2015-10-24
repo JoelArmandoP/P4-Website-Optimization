4c1432a3604637781c11fcb16a6cfb9d8061f7ab
Joel Armando <joelarma@gmail.com>
2015-10-24 20:55:35 +0100

    Add grunt generated files

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,64 +6,61 @@ module.exports = function(grunt) {
 		options:{
 		    cssmin: true
 		},
-		files: [
-		    {
-			expand: true,
-			src: ['**/*.html'],
-			cwd: './',
-			dest: 'dist/'
-		    }]
+		files: [{
+		    expand: true,
+		    src: ['**/*.html'],
+		    cwd: './',
+		    dest: 'dist/'
+		}]
 	    }
 	},
 	imagemin: {
-            dist: {
-		options: {
-		    optimizationLevel: 7
-		},
-		files: [
-		    {
-			expand: true,
-			src: ['**/*.{png,jpg,gif}'],
-			cwd: 'img/',
-			dest: 'dist/img/'
-		    }, {
-			expand: true,
-			src: ['**/*.{png,jpg,gif}'],
-			cwd: 'views/images/',
-			dest: 'dist/views/images/'
-		    }]
-            }
+	    dist: {
+    		options: {
+        	    optimizationLevel: 3,
+        	    progressive: true
+    		},
+    		files: [{
+      		    expand: true,
+      		    cwd: 'img/',
+      		    src: ['**/*.{png,jpg,gif}'],
+      		    dest: 'dist/img'
+    		}, {
+      		    expand: true,
+      		    cwd: 'views/images/',
+      		    src: ['**/*.{png,jpg,gif}'],
+      		    dest: 'dist/views/images'
+    		}]
+	    }
 	},
 	uglify: {
             dist: {
-		files: [
-		    {
-			expand: true,
-			src: ['*.js'],
-			cwd: 'js/',
-			dest: 'dist/js/'
-		    }, {
-			expand: true,
-			src: ['*.js'],
-			cwd: 'views/js/',
-			dest: 'dist/views/js/'
-		    }]
+		files: [{
+		    expand: true,
+		    src: ['*.js'],
+		    cwd: 'js/',
+		    dest: 'dist/js/'
+		}, {
+		    expand: true,
+		    src: ['*.js'],
+		    cwd: 'views/js/',
+		    dest: 'dist/views/js/'
+		}]
             }
 	},
 	cssmin: {
             dist: {
-		files: [
-		    {
-			expand: true,
-			src: ['*.css'],
-			cwd: 'css/',
-			dest: 'dist/css/'
-		    }, {
-			expand: true,
-			src: ['*.css'],
-			cwd: 'views/css/',
+		files: [{
+		    expand: true,
+		    src: ['*.css'],
+		    cwd: 'css/',
+		    dest: 'dist/css/'
+		}, {
+		    expand: true,
+		    src: ['*.css'],
+		    cwd: 'views/css/',
 			dest: 'dist/views/css/'
-		    }]
+		}]
             }
 	}
     });
